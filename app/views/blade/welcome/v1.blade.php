a916c8d1ba6f3a6bcee0e33ebede622375281428
Kydz <boatkyd@gmail.com>
2015-04-03 14:07:08 +0800

    hide link

--- app/views/blade/welcome/v1.blade.php
+++ app/views/blade/welcome/v1.blade.php
@@ -16,7 +16,7 @@
         </div>
         <div class="section-mid"></div>
             <div class="menu-container">
-                 <div>{{HTML::linkAction('ArchiveController@showList', 'A', array(''))}}</div>
+                 <div><a href="#">A</a></div>
                  <div><a href="#">G</a></div>
                  <div><a href="#">O</a></div>
             </div>
