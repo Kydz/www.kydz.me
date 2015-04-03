014a204856b1fb9ddcefcec3606fcd2947cbb65a
Kydz <boatkyd@gmail.com>
2015-04-03 09:56:24 +0800

    file path changes

--- app/controllers/MiscFileController.php
+++ app/controllers/MiscFileController.php
@@ -3,19 +3,19 @@
 class MiscFileController extends BaseController {
 
 
-	public function getCSS($path){
-		$path = dirname(__FILE__).'\..\views\css\\'.$path;
-		$contents = File::get($path);
-		$response = Response::make($contents, 200);
-		$response->header('Content-Type', 'text/css');
-		return $response;
-	}
+    public function getCSS($path){
+        $path = dirname(__FILE__).'/../views/css/'.$path;
+        $contents = File::get($path);
+        $response = Response::make($contents, 200);
+        $response->header('Content-Type', 'text/css');
+        return $response;
+    }
 
-	public function getJS($path){
-		$path = dirname(__FILE__).'\..\views\css\\'.$path;
-		$contents = File::get($path);
-		$response = Response::make($contents, 200);
-		$response->header('Content-Type', 'text/javascript');
-		return $response;		
-	}
+    public function getJS($path){
+        $path = dirname(__FILE__).'/../views/js/'.$path;
+        $contents = File::get($path);
+        $response = Response::make($contents, 200);
+        $response->header('Content-Type', 'text/javascript');
+        return $response;
+    }
 }
