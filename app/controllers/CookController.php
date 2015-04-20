aa2fa409f4bf4fb9bac111455cf0d7de098566a9
Kydz <boatkyd@gmail.com>
2015-04-20 19:51:05 +0800

    article model added

--- app/controllers/CookController.php
+++ app/controllers/CookController.php
@@ -5,4 +5,10 @@ class CookController extends BaseController{
     public function write(){
         return View::make('blade.kitchen.write');
     }
+
+    public function save(){
+        $re = Input::all();
+        $article = new Article();
+        var_dump($re, $article);
+    }
 }
