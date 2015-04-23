86f29b94496d7f453fb935fe98e05aa582a7cd98
Kydz <boatkyd@gmail.com>
2015-04-23 20:37:16 +0800

    detail page

--- app/views/blade/archive/list.blade.php
+++ app/views/blade/archive/list.blade.php
@@ -3,11 +3,13 @@
 <div class="archive_list">
     @foreach($articles as $article)
         @if($article->content != null)
+        <a href="{{Config::get('app.url')}}archive/{{$article->id}}.html">
             <article class="archive_itme">
-                <header>{{{$article->content->title}}}</header>
-                <section>{{{$article->content->brief}}}</section>
-                <footer><span>{{{$article->created_at}}}</span><span>{{{$article->hit}}}</span><span></span></footer>
+                <header>{{$article->content->title}}</header>
+                <section>{{$article->content->brief}}</section>
+                <footer><span>{{$article->created_at}}</span><span>{{$article->hit}}</span><span class="like"></span></footer>
             </article>
+        </a>
         @endif
     @endforeach
 </div>
