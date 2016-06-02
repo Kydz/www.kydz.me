8bcc718b0d09f819d4fcd51cbdeb8ec4cc1d0cc3
Kydz <boatkyd@gmail.com>
2016-06-03 00:01:04 +0800

    updated`

--- resources/views/kitchen/archive/detail.blade.php
+++ resources/views/kitchen/archive/detail.blade.php
@@ -1,41 +1,34 @@
 @extends('kitchen.archive.layout')
+
+@section('head-css')
+<link rel="stylesheet" type="text/css" href="/css/editor.css">
+@stop
+
 @section('main')
-<div>
-    <form action="/archive/{{$article->id or 0}}" method="POST" enctype="multipart/form-data">
-        {!!csrf_field()!!}
-        <div class="form-control"><section>Title:</section><input name="title" value="{{$article->title or ''}}" /></div>
-        <div class="form-control"><section>Brief:</section><input name="brief" value="{{$article->brief or ''}}" /></div>
-        <div class="form-control"><section>Content:</section></div>
-        <div class="form-control"><textarea name="content" id="content" cols="100" rows="40">{{$article->content or ''}}</textarea></div>
-        <div class="form-control"><section>Set to active:<input type="checkbox" name="active" value="1" 
-        @if ($article->active == 1)
-            checked="checked" 
-        @endif
-        /></section></div>
-        <div class="form-control"><section><input type="submit" value="Submit" /><input type="button" id="btn-preview" value="preview" /></section></div>
-        <input type="hidden" value="1" name="type" />
-    </form>
-    <dir id="preview"></dir>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span12">
+            <form>
+                <fieldset>
+                     <legend>表单项</legend> <label>表签名</label><textarea name="content" id="content" cols="100" rows="40">{{$article->content or ''}}</textarea> <span class="help-block">这里填写帮助信息.</span> <label class="checkbox"><input type="checkbox" /> 勾选同意</label> <button type="submit" class="btn">提交</button>
+                </fieldset>
+            </form>
+        </div>
+    </div>
 </div>
 @stop
 
 @section('js')
+<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/zepto/1.0/zepto.min.js"></script>
+<script type="text/javascript" src="/js/editor.js"></script>
+<script type="text/javascript" src="/js/marked.js"></script>
 <script type="text/javascript">
 $(document).ready(function () {
-    $('#btn-preview').on('click', function () {
-        $.ajax({
-            url: '/archive/preview',
-            method: 'POST',
-            data: {content: $('#content').val(), _token: '{{csrf_token()}}'},
-            success: function (re) {
-                if (re.code == 2000) {
-                    $('#preview').html(re.data);
-                } else {
-                    alert(re.message);
-                }
-            }
-        });
+    var markdown = $('#content').val();
+    var editor = new Editor({
+        element: $('#content')[0]
     });
+    editor.render();
 });
 </script>
 @stop
